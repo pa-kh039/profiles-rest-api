6c6ca1a50b99614e699549c186dabebf8c30672f
PARTH KHANDENATH <parthkhandenath123@gmail.com>
2023-02-18 00:06:24 +0530

    added viewset to profiles api

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from profiles_api import views 
 
 
+router=DefaultRouter()
+router.register('hello-viewset',views.HelloViewSet,basename='hello-viewset') #a route has been registered, all the urls can be retrieved using the base_name using the url retrieval function provided by django
+
 urlpatterns=[
-    path('hello-view/',views.HelloApiView.as_view()),
-    
+    path('hello-view/',views.HelloApiView.as_view()), #routes directly inserted inside urlpatterns won't be displayed...routes registered with router and then inserted into urlpatterns will be displayed using defaultrouter, like the below path
+    path('',include(router.urls)),
 ]
+
